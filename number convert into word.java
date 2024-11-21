ed1ec6e0ba8276a9035ab6784ecae07fbfaa7364
KrishnabharathiSakthivel <smkrishnabharathi@gmail.com>
2024-11-21 17:44:34 +0530

    Take a number between 0 to 9 as input and print the word representation for that number.
    Take a number between 0 to 9 as input and print the word representation for that number.
    
    ex:
    input: 7
    output : seven

---
+++ number convert into word.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      Scanner scan=new Scanner(System.in);
+      int a=scan.nextInt();
+      if(a==1)
+      {
+        System.out.println("one");
+      }
+      else if(a==2)
+      {
+        System.out.println("two");
+      }
+      else if(a==3)
+      {
+        System.out.println("three");
+      }
+      else if(a==4)
+      {
+        System.out.println("four");
+      }
+      else if(a==5)
+      {
+        System.out.println("five");
+      } 
+      else if(a==6)
+      {
+        System.out.println("six");
+      }
+      else if(a==7)
+      {
+        System.out.println("seven");
+      }
+      else if(a==8)
+      {
+        System.out.println("eight");
+      }
+      else
+      {
+        System.out.println("nine");
+      }
+  }
+}

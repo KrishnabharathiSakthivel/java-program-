4ed2ce2e61931f343267396cc91b0a2abaaeafed
KrishnabharathiSakthivel <smkrishnabharathi@gmail.com>
2024-11-28 15:24:57 +0530

    find  weekday
    Take an integer ranging from 0 - 6 as input and print out the corresponding weekday. 0 corresponds to Sunday and 6 corresponds to Saturday.
    
    Constraints:
    0 <= input <= 6
    
    Input:
    A single integer from 0 to 6
    
    Output
    A single line with string containing the weekday corresponding to the input.
    0 -> Sunday
    1 -> Monday
    2 -> Tuesday
    3 -> Wednesday
    4 -> Thursday
    5 -> Friday
    6 -> Saturday
    
    Example:
    Input:
    1
    Output:
    Monday

---
+++ weekday.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+     Scanner scan=new Scanner(System.in);
+     int n=scan.nextInt();
+     String []day={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+     System.out.println(day[n]);
+  }
+}
